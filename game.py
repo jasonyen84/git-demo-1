550233abc257829301840761a04733d0f012fc22
Leo <leo@gmail.com>
2024-07-13 14:52:32 +0800

    簡化次數

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 import random
 
-times = 0
 
 x = random.randint(1, 50)
 print(x)
@@ -8,15 +7,17 @@ for i in range(5):
     y = int(input("請猜一數字:"))
     if y == x:
         print("猜對了!")
-        print(f"猜了{times}次")
+
         break
     else:
         if x > y:
             print("猜高一點")
-            times += 1
+
         else:
             print("猜低一點")
-            times += 1
+
 
 if x != y:
     print(f"答案為:{x}")
+else:
+    print(f"共猜了{i+1}次")
