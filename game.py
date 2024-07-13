78ad2d57b572d9f87bf6ab25e0059e7998a7ee35
Leo <leo@gmail.com>
2024-07-13 14:46:01 +0800

    加入計算猜題次數

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import random
 
-right = True
+times = 0
 
 x = random.randint(1, 50)
 print(x)
@@ -8,12 +8,15 @@ for i in range(5):
     y = int(input("請猜一數字:"))
     if y == x:
         print("猜對了!")
+        print(f"猜了{times}次")
         break
     else:
         if x > y:
             print("猜高一點")
+            times += 1
         else:
             print("猜低一點")
+            times += 1
 
 if x != y:
     print(f"答案為:{x}")
